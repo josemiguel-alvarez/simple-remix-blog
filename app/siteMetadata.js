839989b170bdf15a36c357b7e71fad2830f31955
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-09-17 22:12:00 +0200

    Add feed.xml route

--- app/siteMetadata.js
+++ app/siteMetadata.js
@@ -2,13 +2,14 @@ export const siteMetadata = {
   title: "simple-remix-blog",
   description:
     "This a Remix template for a blog built using MDX files. Check the README on GitHub to see how to build your own blog using this template.",
-  domain: "simple-remix-blog",
-  url: "https://github.com/josemiguel-alvarez/simple-remix-blog",
+  domain: "simple-remix-blog.vercel.app",
+  url: "https://simple-remix-blog.vercel.app",
   image: "https://www.jmalvarez.dev/images/banner.png",
   author: "José Miguel Álvarez Vañó",
   twitter: "jmalvarezdev",
   github: "josemiguel-alvarez",
   linkedin: "jmalvarezvano",
+  email: "jm.alvarez.vano@gmail.com",
   professionalTitle: "Software Engineer @ Shopify",
   aboutMe:
     "I'm a Software Engineer @ Shopify. More than 5 years of experience developing software. International working experience, mainly in the Swiss and Spanish banking sector. Full-Stack knowledge with a focus on web development. Interested in new technologies and cybersecurity.",
