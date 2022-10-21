9d1c62cf294c54cfa4319ef8c06a7ac78c224a59
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-10-21 22:02:20 +0200

    Add dark mode styles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -4,11 +4,17 @@ import { InternalLink } from "./InternalLink";
 export const Card = ({ title, summary, slug, formattedDate }: Frontmatter) => {
   return (
     <div>
-      <div className="text-sm text-gray-500">Published {formattedDate}</div>
-      <span className="text-2xl font-bold text-gray-700">{title}</span>
-      <p className="text-lg text-gray-500">{summary}</p>
+      <div className="text-sm text-gray-500 dark:text-gray-400">
+        Published {formattedDate}
+      </div>
+      <span className="text-2xl font-bold text-gray-700 dark:text-gray-200">
+        {title}
+      </span>
+      <p className="text-lg text-gray-500 dark:text-gray-400">{summary}</p>
       <InternalLink to={`/posts/${slug}`}>
-        <span className="font-medium text-black">Read more</span>
+        <span className="font-medium text-black dark:text-white">
+          Read more
+        </span>
       </InternalLink>
     </div>
   );
