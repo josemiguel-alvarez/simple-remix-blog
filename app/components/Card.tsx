6abcc7c7e111dc0100befd62fe600f00440e8c2d
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-10-21 22:07:07 +0200

    Make text darker on light mode

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -7,10 +7,10 @@ export const Card = ({ title, summary, slug, formattedDate }: Frontmatter) => {
       <div className="text-sm text-gray-500 dark:text-gray-400">
         Published {formattedDate}
       </div>
-      <span className="text-2xl font-bold text-gray-700 dark:text-gray-200">
+      <span className="text-2xl font-bold text-gray-800 dark:text-gray-200">
         {title}
       </span>
-      <p className="text-lg text-gray-500 dark:text-gray-400">{summary}</p>
+      <p className="text-lg text-gray-600 dark:text-gray-400">{summary}</p>
       <InternalLink to={`/posts/${slug}`}>
         <span className="font-medium text-black dark:text-white">
           Read more
