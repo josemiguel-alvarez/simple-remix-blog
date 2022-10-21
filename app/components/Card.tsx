f907e090dada8992a3d7028c197527b39a9e1ede
José Miguel Álvarez Vañó <89982193+josemiguel-alvarez@users.noreply.github.com>
2022-10-21 22:09:06 +0200

    feat: Change Card design (#27)
    * Change Card design
    
    * Add dark mode styles
    
    * Make text darker on light mode

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,35 +1,21 @@
-import { Link } from "@remix-run/react";
 import type { Frontmatter } from "~/utils/posts.server";
+import { InternalLink } from "./InternalLink";
 
-interface Props extends Frontmatter {
-  index: number;
-}
-
-export const Card = ({ title, summary, index, slug, formattedDate }: Props) => {
+export const Card = ({ title, summary, slug, formattedDate }: Frontmatter) => {
   return (
-    <Link
-      className={`relative block p-8 overflow-hidden border border-gray-200 rounded-lg ${
-        index % 2 === 0 ? "sm:mr-4" : ""
-      } h-full hover:border-gray-400 dark:hover:border-slate-900 dark:bg-white dark:border-slate-900`}
-      to={`/posts/${slug}`}
-      prefetch="intent"
-    >
-      <span className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
-
-      <div className="justify-between sm:flex">
-        <div>
-          <h5 className="text-xl font-bold text-gray-900">{title}</h5>
-        </div>
+    <div>
+      <div className="text-sm text-gray-500 dark:text-gray-400">
+        Published {formattedDate}
       </div>
-
-      <div className="mt-4 mb-14 sm:pr-8">
-        <p className="text-sm text-gray-500">{summary}</p>
-      </div>
-
-      <div className="flex flex-col-reverse absolute bottom-7">
-        <dt className="text-sm font-medium text-gray-600">Published</dt>
-        <dd className="text-xs text-gray-500">{formattedDate}</dd>
-      </div>
-    </Link>
+      <span className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+        {title}
+      </span>
+      <p className="text-lg text-gray-600 dark:text-gray-400">{summary}</p>
+      <InternalLink to={`/posts/${slug}`}>
+        <span className="font-medium text-black dark:text-white">
+          Read more
+        </span>
+      </InternalLink>
+    </div>
   );
 };
