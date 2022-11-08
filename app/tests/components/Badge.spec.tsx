5b659ef237ef9e3885587017d4a6ef1a2e42d95d
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-11-08 20:00:13 +0100

    Update Badge tests

--- app/tests/components/Badge.spec.tsx
+++ app/tests/components/Badge.spec.tsx
@@ -3,17 +3,6 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
 describe("Badge", () => {
-  test("renders correctly", () => {
-    const { container } = render(
-      <Badge label="Test Badge" linkTo="http://example.com" />,
-      {
-        wrapper: BrowserRouter,
-      }
-    );
-
-    expect(container).toMatchSnapshot();
-  });
-
   test("renders a link with a label", () => {
     render(<Badge label="Test Badge" linkTo="http://example.com" />, {
       wrapper: BrowserRouter,
